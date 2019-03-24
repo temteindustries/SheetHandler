7e70e7edcf73072249ed29842de04278b16bd0f1
jtemte3 <jtemte3@gmail.com>
2019-03-23 22:02:53 -0500

    initial commit

---
+++ src/main/java/com/temteindustries/sheethandler/SheetHandlerApplication.java
@@ -0,0 +1,13 @@
+package com.temteindustries.sheethandler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SheetHandlerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SheetHandlerApplication.class, args);
+	}
+
+}
